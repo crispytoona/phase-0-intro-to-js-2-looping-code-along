cd753a4fb52eb99ff31bbd7443dc7da43a810ee9
Christine Eriksen <christine.eriksen@gmail.com>
2022-12-20 19:06:15 -0500

    commit tree 0c1ae521819eb6a47fdd5aba326bb96066f6f63b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const cardNames = ["Guadalupe", "Ollie", "Aki"]
+const cardEvent = ["surprise"]
+
+function writeCards(cardNames){
+    const card = []
+    for (let i = 0; i < cardNames.length; i++) {
+    card.push(`Thank you, ${cardNames[i]}, for the wonderful ${cardEvent} gift!`)
+    }
+    return card
+}
+
+writeCards(cardNames);
+//
+
+let numero = 10
+
+function countDown(numero){
+    for (let countDown = 10; countDown > -1; countDown--){
+    console.log(countDown);
+    }
+    return numero
+}
+
+countDown(numero)
